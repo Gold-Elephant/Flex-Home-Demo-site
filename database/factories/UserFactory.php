707c414b63592d5904bce1e126b77d582c67fb64
razinsizov <84234127+razinsizov@users.noreply.github.com>
2021-07-06 09:46:37 +0700

    repare

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
-{
+{ 
     /**
      * The name of the factory's corresponding model.
      *
